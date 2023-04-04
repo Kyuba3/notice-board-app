1ccd09fe489ce5e0bc70f60b56c2cbffbd8114d4
Kuba Kołodziej <jmk.kolodziej@gmail.com>
2023-04-04 16:37:42 +0200

    change package.json scripts

--- db.js
+++ db.js
@@ -1,25 +1,30 @@
 const mongoose = require('mongoose');
 
 const connectToDB = () => {
+  // connect to DB
+  const NODE_ENV  = process.env.NODE_ENV;
+  let dbURI = '';
+  if (NODE_ENV === 'production') {
+    dbURI = `mongodb+srv://Kyuba3:${process.env.DB_PASS}@cluster0.lsrtv0c.mongodb.net/adsDb?retryWrites=true&w=majority`;
+  } else if (NODE_ENV === 'test') {
+    dbURI = 'mongodb://localhost:27017/adsDbTest';
+  } else {
+    dbURI = 'mongodb://localhost:27017/adsDb';
+  }
 
-  const NODE_ENV = process.env.NODE_ENV;
-
-  if( NODE_ENV === 'production' ) 
-  dbatlas = `mongodb+srv://Kyuba3:7xSqImSaMkKcojKH@cluster1.ntzrjab.mongodb.net/?retryWrites=true&w=majority`;
-  else if( NODE_ENV === 'test' ) dbatlas = 'mongodb://localhost:27017/adsDbTest';
-  else dbatlas = 'mongodb://localhost:27017/adsDb';
-
-  mongoose.connect(dbatlas, {useNewUrlParser: true});
-
+  mongoose.connect(dbURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
   const db = mongoose.connection;
 
+  // on success
   db.once('open', () => {
-    console.log('Connected to db');
+    console.log('Connected to the database');
   });
 
-  db.on('error', (err) => {
-    console.log('Error ' + err);
-  });
+  // on error
+  db.on('error', (err) => console.log(`Error: ${err}`));
 };
 
 module.exports = connectToDB;
